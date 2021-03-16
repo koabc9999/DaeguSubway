160e5606b2ce280528190928aece3f137734f445
koabc9999 <koabc9999@gmail.com>
2021-03-16 15:10:19 +0900

    JS code check

---
+++ logic.js
@@ -0,0 +1,9 @@
+// 2021 / 3 / 16 화요일 JavaScript 연습
+//
+
+let buttom;
+
+function buttonPressed(){
+    console.log("button pressed\n\n");
+    buttom = document.getElementById('buttom').innerHTML += "the Button pressed<br>function working<br>"
+}
